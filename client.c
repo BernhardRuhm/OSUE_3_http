21155d3d9d6e3fed5d858315900044e1c0cab28d
BernhardRuhm <berni.ruhm@gmail.com>
2021-12-21 17:56:54 +0100

    setupt socket, request, read (validate header in progress)

--- client.c
+++ client.c
@@ -7,14 +7,81 @@
 #include <unistd.h>
 #include <errno.h>
 
+#define DEFAULT_PORT "http"
+#define DEFAULT_FILE "index.html"
+
 char *myprog;
 
-void usage()
+static void usage()
 {
     fprintf(stderr, "usage %s: %s [-p PORT] [ -o FILE | -d DIR ] URL\n", myprog, myprog);
     exit(EXIT_FAILURE);
 }
 
+static void error_exit(char *msg)
+{
+    fprintf(stderr, "%s: %s", myprog, msg);
+    exit(EXIT_FAILURE);
+}
+
+static int setup_socket(char *host, char *port)
+{
+    struct addrinfo hints, *ai;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int res = getaddrinfo(host, port, &hints, &ai);
+    if (res != 0)
+    {
+        fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(res));
+        return -1;
+    }
+    int sockfd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+    if (sockfd < 0)
+    {
+        fprintf(stderr, "socket failed: %d\n", errno);
+        return -1;
+    }
+
+    if (connect(sockfd, ai->ai_addr, ai->ai_addrlen) < 0)
+    {
+    }
+    freeaddrinfo(ai);
+    return sockfd;
+}
+
+
+static void send_request(char *path, char *host, FILE *s)
+{
+    fprintf(s,"GET /%s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", path, host);
+    fflush(s);
+}
+
+static void validate_header(FILE *s)
+{
+    char *line = NULL;
+    size_t len = 0;
+
+    if(getline(&line, &len, s) == -1)
+        error_exit("read from socket failed");
+
+    char *tmp1 = strsep(&line, " ");
+    char *tmp2 = strsep(&line, " ");
+
+}
+
+static void read_and_write(FILE *s, FILE *f)
+{
+    char *line = NULL;
+    size_t len = 0;
+
+    while(getline(&line, &len, s) != -1){
+        fprintf(f, "%s", line);
+    }
+
+    free(line);
+}
 
 int main(int argc, char **argv)
 {
@@ -24,7 +91,6 @@ int main(int argc, char **argv)
     char *port = NULL;
     char *file = NULL;
     char *dir = NULL;
-    char *url = NULL;
     int opt_p = 0;
     int opt_o = 0;
     int opt_d = 0;
@@ -59,34 +125,28 @@ int main(int argc, char **argv)
     if((argc - (2 * opt_p) - (2 * opt_o) - (2 * opt_d)) != 2 )
         usage();
 
+    char *url = argv[optind];
 
-
-
-    /*
-    struct addrinfo hints, *ai;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-
-    int res = getaddrinfo("neverssl.com", "http", &hints, &ai);
-    if (res != 0)
+    if(strncmp(url, "http://", 7) != 0)
     {
-        fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(res));
-        exit(EXIT_FAILURE);
+        error_exit("url must contain http://\n");
     }
-    int sockfd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
-    if (sockfd != 0)
+    char *url_wihtout_http = url + 7;
+    char *host = strsep(&url_wihtout_http, ";/?:@=&");
+    char *path = url_wihtout_http;
+   
+    int sockfd = setup_socket(host, DEFAULT_PORT);
+    if(sockfd == -1)
     {
-        fprintf(stderr, "socket failed: %d\n", errno);
-    }
-
-    if (connect(sockfd, ai->ai_addr, ai->ai_addrlen) < 0)
-    {
+        error_exit("creating socket failed\n");
     }
 
     FILE *sockfile = fdopen(sockfd, "r+");
+    send_request(path, host, sockfile);
 
-    freeaddrinfo(ai);
-    */
+    validate_header(sockfile);
+    //read_and_write(sockfile, stdout);
+    fclose(sockfile);
+    
     return EXIT_SUCCESS;
 }
